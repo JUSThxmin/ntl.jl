43f17bc31d064523e8a1b662a2450c015b0e6ed1
Chuck <45850746+Chuck0618@users.noreply.github.com>
2020-07-11 23:21:10 +0800

    Update lzz_pX.jl

--- lzz_pX.jl
+++ lzz_pX.jl
@@ -1,15 +1,47 @@
 include("lzz_p.jl")
 
+## vector of zz_p{T}
+vec_zz_p{T} = Vector{zz_p{T}} where {T} 
+
+function convert(::Type{vec_zz_p{T}}, v::Vector{Int}) where {T}
+    return map(zz_p{T}, v)
+end
+
+## polynomial with coefficients in zz_p{T}
+
 mutable struct zz_pX{T}
     _rep::vec_zz_p{T}
     function zz_pX{T}(X::Int) where {T}
-        x = convert(zz_p{T}, X)
-        # y= [x]
-        # println("-------->", x,"----->",y)
-        # println(typeof(y))
-        new( [x])
+        x = zz_p{T}(X)
+        new([x])
     end
     function zz_pX{T}(x::zz_p{T})  where {T}
         new([x])
     end
+    function zz_pX{T}(v::vec_zz_p{T}) where {T}
+        new(v)
+    end
+    function zz_pX{T}(v::Array{Int64,1}) where {T}
+        new(v)
+    end
+end
+
+"""
+show(io::IO, p::zz_pX{T}, x::Symbol) 
+
+show the polynomial p in variable x
+julia> p = zz_pX{7}([1,2,3])
+julia> show(stdout, p, :x)
+"""
+function show(io::IO, p::zz_pX{T}, sym::Symbol) where {T}
+    coef = p._rep
+    print(io, coef[1])
+    x = string(sym)
+    len = length(p._rep)
+    for i = 2:len
+        if !iszero(coef[i])
+        print(io, "+", coef[i], "*", x, "^", i-1)
+        end
+    end
+    return nothing
 end
