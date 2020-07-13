846858d921b21e8ff128f02fb96feb99d599e528
Chuck <45850746+Chuck0618@users.noreply.github.com>
2020-07-13 23:13:15 +0800

    Update lzz_pX.jl

--- lzz_pX.jl
+++ lzz_pX.jl
@@ -1,4 +1,6 @@
 include("lzz_p.jl")
+import Base: getindex, zero, one
+
 
 ## vector of zz_p{T}
 vec_zz_p{T} = Vector{zz_p{T}} where {T} 
@@ -15,14 +17,19 @@ mutable struct zz_pX{T}
         x = zz_p{T}(X)
         new([x])
     end
+    function zz_pX{T}() where {T}
+        return zz_pX{T}(0)
+    end
     function zz_pX{T}(x::zz_p{T})  where {T}
         new([x])
     end
     function zz_pX{T}(v::vec_zz_p{T}) where {T}
-        new(v)
+        p=new(v)
+        normalize!(p)
     end
     function zz_pX{T}(v::Array{Int64,1}) where {T}
-        new(v)
+        p=new(v)
+        normalize!(p)
     end
 end
 
@@ -45,3 +52,69 @@ function show(io::IO, p::zz_pX{T}, sym::Symbol) where {T}
     end
     return nothing
 end
+function normalize!(p::zz_pX{T})::zz_pX{T} where {T}
+    r = p._rep
+    m= length(r)
+    while (m > 1) & iszero(r[m])
+        pop!(r)
+        m -= 1
+    end
+    return p
+end
+
+function deg(p::zz_pX{T}) where {T}
+    return length(p._rep) - 1
+end
+function coeff(p::zz_pX{T}, i::Int) where {T}
+    if i < 0
+        error("coeff: negative index")
+    end
+    if i in 0:deg(p)
+        return p._rep[i+1]
+    end
+    return zz_p{T}(0)
+end
+
+function zero(::Type{zz_pX{T}}) where {T}
+    return zz_p{T}(0)
+end
+function one(::Type{zz_pX{T}}) where {T}
+    return zz_p{T}(1)
+end
+
+function GetCoeff!(x::zz_p{T}, p::zz_pX{T}, i::Int) where {T}
+    x = coeff(p,i)
+end
+function LeadCoeff(p::zz_pX{T}) where {T} 
+    p._rep[end] 
+end
+function ConstTerm(p::zz_pX{T}) where {T}
+    p._rep[1]
+end
+function getindex(p::zz_pX{T}, i::Int) where {T}
+    if i < 0
+        error("getindex: negative index")
+    end
+    if i in 0:deg(p)
+        return getindex(p._rep, i+1)
+    end
+    return zero(zz_pX{T})
+end
+
+function setindex!(p::zz_pX{T}, i::Int, x::zz_p{T}) where {T}
+    if i < 0
+        error("setindex!: nagative index")
+    end
+    m = deg(p)
+    if (i > m) & !(iszero(x)) 
+        return x
+    end
+    if i >m
+        z=zeros(zz_p{T},i-m)
+        append!(p._rep, z)
+    end
+    setindex!(p._rep, i, x)
+    normalize!(p)
+end
+
+
